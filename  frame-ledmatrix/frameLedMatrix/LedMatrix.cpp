6d8afc10e7b7a0c55a1b8acd2b40522b4222402c
pvvinh8889@gmail.com <pvvinh8889@gmail.com@05697e86-51f0-57be-3be2-4d1ca2f99d55>
2013-08-14 01:14:20 +0000

    add file, change way to library

---
+++  frame-ledmatrix/frameLedMatrix/LedMatrix.cpp
@@ -0,0 +1,16 @@
+
+
+#include "Arduino.h"
+#include "LedMatrix.h"
+
+LedMatrix::LedMatrix(byte lat, byte selectA, byte selectB, byte int1, byte int2){
+
+}
+
+LedMatrix::scan_led(){
+
+}
+
+LedMatrix::fill_led(byte mask){
+
+}
