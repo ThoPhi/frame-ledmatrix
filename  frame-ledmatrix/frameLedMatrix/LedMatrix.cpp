b014d39fa4007c821ee1a7f2268e664339c1bc7b
pvvinh8889@gmail.com <pvvinh8889@gmail.com@05697e86-51f0-57be-3be2-4d1ca2f99d55>
2013-08-22 11:12:09 +0000

    Đang viết dạng thư viện C++

---  frame-ledmatrix/frameLedMatrix/LedMatrix.cpp
+++  frame-ledmatrix/frameLedMatrix/LedMatrix.cpp
@@ -1,16 +1,94 @@
-
-
 #include "Arduino.h"
 #include "LedMatrix.h"
+#include <SPI.h>
 
-LedMatrix::LedMatrix(byte lat, byte selectA, byte selectB, byte int1, byte int2){
 
+LedMatrix::LedMatrix(byte enPin1, byte enPin2, byte selectA, byte selectB, byte lat, byte sizeMatrix){
+  _lat = lat;
+  _selectA = selectA;
+  _selectB = selectB;
+  _enPin1 = enPin1;
+  _enPin2 = enPin2;
+  _size = sizeMatrix;
+  pinMode(lat, OUTPUT);  // lat pin of SPI
+  digitalWrite(lat,LOW);
+  pinMode(selectA,OUTPUT);  // two select pins to select row display
+  digitalWrite(selectA,LOW);
+  pinMode(selectB, OUTPUT);
+  digitalWrite(selectB,LOW);
+  pinMode(enPin1,OUTPUT);  //
+  digitalWrite(enPin1,LOW);
+  pinMode(enPin2,OUTPUT);  //
+  digitalWrite(enPin2,LOW);
+  SPI.begin();
+}
+
+LedMatrix::LedMatrix(byte enPin1, byte enPin2, byte selectA, byte selectB, byte lat){
+  _lat = lat;
+  _selectA = selectA;
+  _selectB = selectB;
+  _enPin1 = enPin1;
+  _enPin2 = enPin2;
+  pinMode(lat, OUTPUT);  // lat pin of SPI
+  digitalWrite(lat,LOW);  
+  pinMode(selectA,OUTPUT);  // two select pins to select row display
+  digitalWrite(selectA,LOW);
+  pinMode(selectB, OUTPUT);
+  digitalWrite(selectB,LOW);
+  pinMode(enPin1,OUTPUT);  //
+  digitalWrite(enPin1,LOW);
+  pinMode(enPin2,OUTPUT);  //
+  digitalWrite(enPin2,LOW);
+  SPI.begin();
+}
+
+LedMatrix::LedMatrix(){
+  _lat = 10;
+  _selectA = 6;
+  _selectB = 7;
+  _enPin1 = 8;
+  _enPin2 = 9;
+  pinMode(_lat, OUTPUT);  // lat pin of SPI
+  digitalWrite(_lat,LOW);
+  pinMode(_selectA,OUTPUT);  // two select pins to select row display
+  digitalWrite(_selectA,LOW);
+  pinMode(_selectB, OUTPUT);
+  digitalWrite(_selectB,LOW);
+  pinMode(_enPin1,OUTPUT);  //
+  digitalWrite(_enPin1,LOW);
+  pinMode(_enPin2,OUTPUT);  //
+  digitalWrite(_enPin2,LOW);
+  SPI.begin();
 }
 
 LedMatrix::scan_led(){
-
+  byte tmp = _rowSelect %4;
+  digitalWrite(_lat,LOW);
+  digitalWrite(_enPin2,LOW);
+  digitalWrite(_enPin1,LOW);
+  if(_rowSelect <4){
+    for(j=0;j<16;j++){
+      SPI.transfer(~bufferReceive[j/4][((15-j)%4)*4 + tmp]);
+    }
+    digitalWrite(SelectB,rowSelect%2);
+    digitalWrite(SelectA,rowSelect/2);
+    digitalWrite(LAT, HIGH);
+    digitalWrite(enPin1,HIGH);
+  }
+  else{
+    for(j=0;j<16;j++){
+      SPI.transfer(~bufferReceive[j/4][((15-j)%4)*4 + tmp + 16]);
+    }
+    digitalWrite(SelectB,tmp%2);
+    digitalWrite(SelectA,tmp/2);
+    digitalWrite(LAT, HIGH);
+    digitalWrite(enPin2,HIGH);
+  }
+  rowSelect = ((rowSelect+1)%8);
 }
 
 LedMatrix::fill_led(byte mask){
 
 }
+
+
---  frame-ledmatrix/frameLedMatrix/LedMatrix.h
+++  frame-ledmatrix/frameLedMatrix/LedMatrix.h
@@ -3,11 +3,21 @@
 #ifndef LedMatrix_h
 #define LedMatrix_h
 
-#include "Arduino.h"
+//#include "Arduino.h"
+#define SIZE 32
 
 class LedMatrix{
-  public:
-  
-  private:
+public:
+  void LedMatrix(byte enPin1, byte enPin2, byte selectA, byte selectB, byte Lat, byte sizeMatrix);
+  void LedMatrix(byte enPin1, byte enPin2, byte selectA, byte selectB, byte Lat);
+  void LedMatrix();
+  void scanLedMatrix(byte mode);
+  void scanLedMatrix();
+private:
+  //  byte _size = 32;
+  byte _lat, _selectA, _selectB, enPin1, _enPin2;
+  byte _rowSelect;
+  byte _bufferReceive[SIZE/8][SIZE];
 };
 #endif
+
